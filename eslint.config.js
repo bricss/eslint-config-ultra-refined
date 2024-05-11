c0a34201b3bc15f2676f7f7f1636f359e167b9bb
Yahor Siarheyenka <yehor.sergeenko@gmail.com>
2024-05-11 11:15:03 +0200

    fix: up 🪛

--- eslint.config.js
+++ eslint.config.js
@@ -398,6 +398,7 @@ export default [
         {
           ignoreConsecutiveComments: true,
           ignoreInlineComments: true,
+          ignorePattern: 'pragma|ignored',
         },
       ],
       curly: [
